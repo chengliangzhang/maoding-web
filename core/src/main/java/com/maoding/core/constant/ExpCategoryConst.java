ffe91cb282db52f7697e13dc7825878a2766782f
svn <svn@imaoding.com>
2018-07-13 17:05:15 +0800

    从svn更新

---
+++ core/src/main/java/com/maoding/core/constant/ExpCategoryConst.java
@@ -0,0 +1,44 @@
+package com.maoding.core.constant;
+
+/**
+ * 作    者 : DongLiu
+ * 日    期 : 2018/2/23 11:32
+ * 描    述 : 费用录入导入
+ */
+public class ExpCategoryConst {
+    public static final String DATE_TIME = "时间";
+    public static final String EXP_SALESTAX_TAX = "主营业务税金及附加";
+    public static final String EXP_DIRECTCOSTS_SALAY = "投入在项目上的员工工资";
+    public static final String EXP_DIRECTCOSTS_FUND = "投入在项目上的五险一金";
+    public static final String EXP_DIRECTCOSTS_BONUS = "投入在项目上的员工奖金";
+    public static final String EXP_EXECUTIVESALARY_SALAY = "管理行政人员的工资";
+    public static final String EXP_EXECUTIVESALARY_FUND = "管理行政人员的五险一金";
+    public static final String EXP_EXECUTIVESALARY_BONUS = "管理行政人员的奖金";
+    public static final String EXP_FWSALARY_BGCD = "办公场地房租";
+    public static final String EXP_FWSALARY_WG = "物管费用";
+    public static final String EXP_FWSALARY_SD = "水电费用";
+    public static final String EXP_FWSALARY_NET = "网络、电话";
+    public static final String EXP_FWSALARY_GBWH = "办公场地维护费用";
+    public static final String EXP_ASSETSAMORTIZATION_BGZX = "办公室装修摊销";
+    public static final String EXP_ASSETSAMORTIZATION_BGSB = "办公室设备摊销";
+    public static final String EXP_ASSETSAMORTIZATION_RJTX = "软件摊销";
+    public static final String EXP_ASSETSAMORTIZATION_BDCZJ = "不动产折旧";
+    public static final String EXP_ZCJZZB_HZZB = "应收账款坏账准备";
+    public static final String EXP_CWFY_SXF = "银行手续费";
+    public static final String EXP_CWFY_LX = "银行利息收入/支出";
+    public static final String EXP_SDSFY_SDS = "减：所得税费用";
+
+
+    public static final String EXP_MAIN_INCOME = "主营业务收入";
+    public static final String EXP_DIRECT_COST = "直接项目成本";
+    public static final String EXP_OTHER_INCOME = "其他业务收入";
+    public static final String EXP_FIXED_COST = "固定支出";
+
+
+
+    public static final String EXP_PROJECT_CONTRACT = "合同回款";
+    public static final String EXP_PROJECT_TECHNICAL = "技术审查费";
+    public static final String EXP_PROJECT_COOPERATIVE = "合作设计费";
+    public static final String EXP_PROJECT_OTHER = "其他收支";
+
+}
