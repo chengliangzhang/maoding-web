9f26725e878ae73ebe99c9851f93860b6a9aa363
svn <svn@imaoding.com>
2018-08-08 16:20:21 +0800

    从svn更新

---
+++ core/src/main/java/com/maoding/core/constant/ProjectCostConst.java
@@ -0,0 +1,15 @@
+package com.maoding.core.constant;
+
+public interface ProjectCostConst {
+
+    //费用申请开始
+    Integer FEE_STATUS_START = 0;
+    //费用审批通过
+    Integer FEE_STATUS_APPROVE = 1;
+    //费用审批拒绝
+    Integer FEE_STATUS_NOT_APPROVE = 2;
+    //费用审批中
+    Integer FEE_STATUS_APPROVE_ING = 5;
+    //撤销
+    Integer FEE_STATUS_REPEAL = 3;
+}
