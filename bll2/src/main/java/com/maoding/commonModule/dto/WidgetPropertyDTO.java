ca1676a13aa09257fc61b2ef53242f0112b9cb5f
zhangchengliang <zhangchengliang@imaoding.com>
2018-09-18 18:07:49 +0800

    修正错误

--- bll2/src/main/java/com/maoding/commonModule/dto/WidgetPropertyDTO.java
+++ bll2/src/main/java/com/maoding/commonModule/dto/WidgetPropertyDTO.java
@@ -19,10 +19,13 @@ public class WidgetPropertyDTO extends CoreShowDTO {
     private String defaultValue;
     
     /** 属性输入框类型：1-文本框,2-单选框,3-多选框,4-列表框 **/
-    private String propertySettingType;
+    private Integer propertySettingType;
 
     /** 属性可选择值，各选择值使用","分隔，如果为空，则可以为任意值 **/
     private String allowValue;
+
+    /** 保存模板时可以通过type_code值进行属性设置：0-不可以,1-可以 **/
+    private Integer canSaveByCode;
 
     public String getPropertyTitle() {
         return propertyTitle;
@@ -40,12 +43,21 @@ public class WidgetPropertyDTO extends CoreShowDTO {
         this.defaultValue = defaultValue;
     }
 
-    public String getPropertySettingType() {
+    public Integer getPropertySettingType() {
         return propertySettingType;
     }
 
-    public void setPropertySettingType(String propertySettingType) {
+    public void setPropertySettingType(Integer propertySettingType) {
         this.propertySettingType = propertySettingType;
+    }
+
+    public Integer getCanSaveByCode() {
+
+        return canSaveByCode;
+    }
+
+    public void setCanSaveByCode(Integer canSaveByCode) {
+        this.canSaveByCode = canSaveByCode;
     }
 
     public String getAllowValue() {
